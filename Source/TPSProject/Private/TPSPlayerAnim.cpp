19784ec8a22beda49575bd9f6f2ec708e0185a69
KIMNAMHYEON <skagus912@gmail.com>
2026-06-08 12:28:39 +0900

    feat: TPSPlayerAnim Direction 변수 분리 + Blend Space 활용

--- Source/TPSProject/Private/TPSPlayerAnim.cpp
+++ Source/TPSProject/Private/TPSPlayerAnim.cpp
@@ -18,8 +18,13 @@ void UTPSPlayerAnim::NativeUpdateAnimation(float DeltaSeconds)
 	ATPSPlayer* player= Cast<ATPSPlayer>(pawn);
 	if (player)
 	{
-		// 속도가 있는 상태인지
-		speed = player->GetVelocity().Size();
+		// 속도, 전후좌우 벡터가져오기
+		FVector velocity = player->GetVelocity();
+		FVector forward = player->GetActorForwardVector();
+		FVector right = player->GetActorRightVector();
+		
+		speed = FVector::DotProduct(velocity, forward); // 앞=양수, 뒤=음수
+		direction = FVector::DotProduct(velocity, right); // 우=양수, 좌=음수
 		
 		// 공중에 떠 있는 상태인지
 		UCharacterMovementComponent* movement = player->GetCharacterMovement();
@@ -29,11 +34,12 @@ void UTPSPlayerAnim::NativeUpdateAnimation(float DeltaSeconds)
 		}
 	}
 	
-	// [DEBUG] 프레임마다 변하는 speed 값 출력용
+	// [DEBUG] 프레임마다 변하는 변수 값 출력용
 	if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(-1,0.f,FColor::Green,
-			FString::Printf(TEXT("Anim Speed: %f"), speed));
+			FString::Printf(TEXT("Speed: %.1f, direction: %.1f, isInAir: %s"), 
+				speed, direction, isInAir ? TEXT("true") : TEXT("false")));
 	}
 	
 	// 
